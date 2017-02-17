13a299538a515b675b57d8b5af6200a1b28995a6
akashrajkn <akashrajkn@gmail.com>
2017-02-17 09:15:56 +0100

    ML - Clustering K Means analysis added

--- JASP-Desktop/mainwindow.cpp
+++ JASP-Desktop/mainwindow.cpp
@@ -74,6 +74,7 @@
 #include "analysisforms/MachineLearning/mlregressionrandomforestform.h"
 #include "analysisforms/MachineLearning/mlregressionboostingform.h"
 #include "analysisforms/MachineLearning/mlregressionknnform.h"
+#include "analysisforms/MachineLearning/mlclusteringkmeansform.h"
 
 
 #include <QDebug>
@@ -808,6 +809,8 @@ AnalysisForm* MainWindow::loadForm(const string name)
 		form = new MLRegressionBoostingForm(contentArea);
 	else if (name == "MLRegressionKNN")
 		form = new MLRegressionKNNForm(contentArea);
+	else if (name == "MLClusteringKMeans")
+		form = new MLClusteringKMeansForm(contentArea);
 #endif
 	else if (name == "NetworkAnalysis")
 		form = new NetworkAnalysisForm(contentArea);
